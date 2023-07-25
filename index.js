f544095df6feb9b78762f3ecfcefe0899dbcde59
Matthew York <myorkprogramming@gmail.com>
2023-07-24 23:39:40 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//1. 
+//Array: Binary Search
+
+/* Given a sorted array and a value, return whether the array contains that value. 
+Do not sequentially iterate the array. Instead, ‘divide and conquer’, 
+taking advantage of the fact that the array is sorted. 
+As always, only use built-in functions that you are prepared to recreate (write yourself) on demand! */
