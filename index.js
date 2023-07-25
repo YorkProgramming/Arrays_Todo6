627bd224024a76c8641c69154a63f18df43f391e
Matthew York <myorkprogramming@gmail.com>
2023-07-24 23:42:13 -0700

    Binary Search

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
 Do not sequentially iterate the array. Instead, ‘divide and conquer’, 
 taking advantage of the fact that the array is sorted. 
 As always, only use built-in functions that you are prepared to recreate (write yourself) on demand! */
+
+function binarySearch(arr, val){
+    var mid = Math.floor(arr.length/2);
+    if (arr[mid] == val){
+        return true;
+    }
+    else if (arr[mid] < val && arr.length > 1){
+        return binarySearch(arr.splice(mid, Number.MAX_VALUE), val);
+    }
+    else if (arr[mid] > val && arr.length > 1){
+        return binarySearch(arr.splice(0, mid), val);
+    }
+    else return false;
+}
+
+//Example:
+var arr = [1,2,3,4,5,6,7,8,9,10];
+console.log(binarySearch(arr, 5)); //true
+console.log(binarySearch(arr, 11)); //false
+
+//#####################################################################################################################
